376bfd239501e2de0bcfb9e5d601b3f5c3839529
Diêgo de Barros <debarrosdiego415@gmail.com>
2023-12-29 01:37:46 -0300

    att

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
 
-async  function resolveprofileData(profileData){
-  //function to fill dynamically the profille data fields
-  const photo = document.getElementById('profile.photo');
-  photo.src = profileData.photo
-  const name = document.getElementById('profile.name');
-  name.innerText = profileData.name
-  const phone = document.getElementById('profile.phone');
-  phone.innerText = profileData.phone
-  const job = document.getElementById('profile.job');
-  job.innerText = profileData.job
-  const email = document.getElementById('profile.email');
-  email.innerText = profileData.email
-}
-
-
-async function resolveHardSkills(profileData){
-  const hardSkills = document.getElementById('profile.skills.hardSkills')
-  hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src='${skill.logo}'/></li>`)
-}
-
-async function resolveSoftSkilss(profileData){
-
-  const softSKills = document.getElementById('profile.skills.softSkills')
-  softSKills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
-}
-
-
-
-(async () => {
+document.addEventListener('DOMContentLoaded', async function() {
+  // Seu código aqui, incluindo a chamada para resolveHardSkills
+  async  function resolveprofileData(profileData){
+    //function to fill dynamically the profille data fields
+    const photo = document.getElementById('profile.photo');
+    photo.src = profileData.photo
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = profileData.phone
+    const job = document.getElementById('profile.job');
+    job.innerText = profileData.job
+    const email = document.getElementById('profile.email');
+    email.innerText = profileData.email
+  }
   
-  const profileData = await fetchProfileData();
-  console.log(profileData);
-  resolveprofileData(profileData);
-  resolveSoftSkilss(profileData);
-  resolveHardSkills(profileData);
-})();
+  async function resolveSoftSkilss(profileData){
+  
+    const softSKills = document.getElementById('profile.skills1.softSkills')
+    softSKills.innerHTML = profileData.skills1.softSkills.map(skill => `<li>${skill}</li>`).join('')
+  }
+
+  async function resolveHardSkills(profileData){
+    const hardSkills = document.getElementById('profile.skills1.hardSkills')
+    if (hardSkills !=null) {
+      hardSkills.innerHTML = profileData.skills1.hardSkills.map(skill => `<li><img src='${skill.logo}'/></li>`).join('');
+    } else {
+      console.error('Elemento com ID "profile.skills.hardSkills" não encontrado no DOM.');
+    }
+  }
+  
+  
+  
+  (async () => {
+    
+    const profileData = await fetchProfileData();
+    console.log(profileData);
+    resolveprofileData(profileData);
+    resolveSoftSkilss(profileData);
+    resolveHardSkills(profileData);
+  })();
+  
+});
