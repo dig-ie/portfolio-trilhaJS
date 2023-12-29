e97d65e69742a7a31845801e1d0d36530c092d0e
Diêgo de Barros <debarrosdiego415@gmail.com>
2023-12-29 00:30:16 -0300

    profile data archive update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 async  function resolveProfileData(profileData){
+  //function to fill dynamically the profille data fields
   const photo = document.getElementById('profile.photo');
   photo.src = profileData.photo
+  const name = document.getElementById('profile.name');
+  name.innerText = profileData.name
+  const phone = document.getElementById('profile.phone');
+  phone.innerText = profileData.phone
+  const job = document.getElementById('profile.job');
+  job.innerText = profileData.job
+  const email = document.getElementById('profile.email');
+  email.innerText = profileData.email
+}
 
+async function resolveSoftSkilss(profiledata){
+
+  const softSKills = document.getElementById('profile.softSkills')
+  softSKills.innerText = profiledata.softSKills
+  
 }
 
 (async () => {
